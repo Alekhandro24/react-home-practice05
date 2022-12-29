20db65c84c0af51d5e9611b1f6aea399df4a61c5
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-12-29 21:32:02 +0200

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+// import { useNavigate } from 'react-router-dom';
+// import FakeAPI from '../fakeAPI';
+
+// export const Login = () => {
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async values => {
+//     const response = await FakeAPI.login(values);
+//     if (response.success) {
+//       navigate('/profile', { replace: true });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Login page</h1>
+//       <LoginForm onSubmit={handleSubmit} />
+//     </div>
+//   );
+// };
